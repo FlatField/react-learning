ee05bd828deaa90634b6c94b993a375239d4ec34
yuta hirano <itrealize@gmail.com>
2021-09-14 13:14:02 +0900

    タスクの追加機能を実装しました

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const onClickAdd = () => {
+    const inputText = document.getElementById("add-text").value;
+    document.getElementById("add-text").value = "";
+
+    //li生成
+    const li = document.createElement("li");
+    li.className = "list-row";
+    
+    //div生成
+    const div = document.createElement("div");
+    div.innerText = inputText;
+
+    //liタグの子要素に各要素を設定
+    li.appendChild(div);
+
+    //未完了のリストに追加
+    document.getElementById("imcomplete-list").appendChild(li);
+}
+
+document
+    .getElementById('add-button')
+    .addEventListener("click", () => onClickAdd());
+
