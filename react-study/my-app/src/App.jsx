3faef357f25fb436f5951b906ce003fd26a32d0a
yuta hirano <itrealize@gmail.com>
2021-09-15 01:27:53 +0900

    useState,useEffectを使って、処理の実行を分岐と細分化する処理を行いました

--- react-study/my-app/src/App.jsx
+++ react-study/my-app/src/App.jsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ColorfullMessage from './components/ColorfullMessage'
 
 
 const App = () => {
-    const onClickButton = () => alert();
+    const [num, setNum] = useState(0);
+    const [FaceShowFlag, setFaceShowFlag] = useState(true);
+
+    const onClickCountUp = () => {
+        setNum(num + 1);
+    };
+    const onClickSwitchShowFlag = () => {
+        setFaceShowFlag(!FaceShowFlag);
+    };
+
+    useEffect(() => {
+        if (num > 0 && num % 3 === 0) {
+            FaceShowFlag || setFaceShowFlag(true);
+        } else {
+            FaceShowFlag && setFaceShowFlag(false);
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[num]);
 
     return (
         <>
             <h1 style={{ color:'red' }}>こんにちは!</h1>
             <ColorfullMessage color="blue">お元気ですか?</ColorfullMessage>
             <ColorfullMessage color="pink">元気です</ColorfullMessage>
-            <button onClick={onClickButton}>ボタン</button>
+            <button onClick={onClickCountUp}>カウントアップ</button>
+            <p>{num}</p>
+            <br />
+            <button onClick={onClickSwitchShowFlag}>on/off</button>
+            {FaceShowFlag && <p>😁</p>}
         </>
     );
 };
